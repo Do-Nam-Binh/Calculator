31b5a274d796f8cbb1ced9b1fe5c5bbef73266e8
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-07-11 14:19:35 +0700

    Add functionality to digit buttons

--- script.js
+++ script.js
@@ -35,3 +35,52 @@ function operate(operator, num1, num2){
     }
     return result;
 }
+
+function clear(){
+    calculation.textContent = "";
+    resultDisplay.textContent = "";
+}
+
+function updateResult(number){
+    resultDisplay.textContent += number;
+}
+
+//Define displays
+const calculation = document.querySelector('.calculation');
+const resultDisplay = document.querySelector('.result');
+
+//Define operator buttons
+const addButton = document.querySelector('.add');
+const substractButton = document.querySelector('.substract');
+const multiplyButton = document.querySelector('.multiply');
+const divideButton = document.querySelector('.divide');
+
+//Define all digits
+const one = document.querySelector('.one');
+one.addEventListener('click', function(){updateResult("1");});
+const two = document.querySelector('.two');
+two.addEventListener('click', function(){updateResult("2");});
+const three = document.querySelector('.three');
+three.addEventListener('click', function(){updateResult("3");});
+const four = document.querySelector('.four');
+four.addEventListener('click', function(){updateResult("4");});
+const five = document.querySelector('.five');
+five.addEventListener('click', function(){updateResult("5");});
+const six = document.querySelector('.six');
+six.addEventListener('click', function(){updateResult("6");});
+const seven = document.querySelector('.seven');
+seven.addEventListener('click', function(){updateResult("7");});
+const eight = document.querySelector('.eight');
+eight.addEventListener('click', function(){updateResult("8");});
+const nine = document.querySelector('.nine');
+nine.addEventListener('click', function(){updateResult("9");});
+const zero = document.querySelector('.zero');
+zero.addEventListener('click', function(){updateResult("0");});
+
+
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', clear);
+const deleteBtn = document.querySelector('.delete');
+
+const decimalBtn = document.querySelector('.decimal');
+
